e49e68f8e8b6371686b8b62c35b5e6e6cdfe381d
Raphael Bernhard <beraphae@gmail.com>
2020-11-26 20:10:29 +0100

    pywsrpc: Simplify API

--- pywsrpc/client.py
+++ pywsrpc/client.py
@@ -1,11 +1,33 @@
 import asyncio
 import json
-from asyncio import Queue
+from asyncio import Queue, QueueFull
 from datetime import datetime
 from uuid import uuid4
 
 import msgpack
 from websockets import connect
+
+RX_QUEUE_SIZE = 1000
+
+
+class Receiver(object):
+    def __init__(self, client):
+        self._queue = Queue(RX_QUEUE_SIZE)
+        self._client = client
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def queue(self) -> Queue:
+        return self._queue
+
+    async def next(self):
+        return await self._queue.get()
+
+    def disconnect(self):
+        self._client.unregister(self)
 
 
 class Client(object):
@@ -14,9 +36,10 @@ class Client(object):
     Incoming messages are send to a `asyncio.Queue`, which is accessible
     over `Client.queue`.
     """
+
     def __init__(self):
-        self._queue = Queue()
         self._ws = None
+        self._receivers = {}
 
     async def connect(self, url):
         """
@@ -36,16 +59,31 @@ class Client(object):
                     msg = msgpack.unpackb(msg)
                 else:
                     msg = json.loads(msg)
-                await self._queue.put(msg)
+                for (flt, receiver) in self._receivers.values():
+                    assert isinstance(receiver, Receiver)
+                    if flt is None or flt(msg):
+                        try:
+                            receiver.queue.put_nowait(msg)
+                        except QueueFull:
+                            pass
             except:
                 continue
         self._ws = None
 
-    async def send_request(self, msg) -> str:
+    def listen(self, flt=None) -> Receiver:
+        rx = Receiver(self)
+        self._receivers[id(rx)] = (flt, rx)
+        return rx
+
+    def unregister(self, rx: Receiver):
+        del self._receivers[id(rx)]
+
+    async def send_request(self, msg, id=None) -> str:
         """
         Send a request, returning the request id.
         """
-        id = str(uuid4())
+        if id is None:
+            id = str(uuid4())
         await self._ws.send(json.dumps({'id': id, 'message': msg}))
         return id
 
@@ -54,22 +92,15 @@ class Client(object):
         Send a request and wait for the answer.
         Returns None if no answer was received during the timeout.
         """
-        id = await self.send_request(msg)
-        start = datetime.now()
-        while True:
-            msg = await asyncio.wait_for(self._queue.get(), timeout)
-            if 'Reply' in msg:
-                rx_id = msg['Reply']['request']
-                if rx_id == id:
-                    return msg['Reply']['message']
-            now = datetime.now()
-            if (now - start).total_seconds() > timeout:
-                break
-        return None
+        id = str(uuid4())
 
-    @property
-    def queue(self):
-        """
-        Provides access to the internal queue of incoming messages.
-        """
-        return self._queue
+        def flt(msg):
+            return 'Reply' in msg and msg['Reply']['request'] == id
+
+        rx = self.listen(flt)
+        await self.send_request(msg)
+        try:
+            return await asyncio.wait_for(rx.next(), timeout)
+        except TimeoutError:
+            pass
+        return None
