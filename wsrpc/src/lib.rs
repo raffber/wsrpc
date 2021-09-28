d5363c82275d5cfe1498c8529620ba9ed6b238d1
Raphael Bernhard <beraphae@gmail.com>
2021-09-28 11:19:54 +0200

    Various small improvements

--- wsrpc/src/lib.rs
+++ wsrpc/src/lib.rs
@@ -2,6 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use std::fmt::{Debug, Formatter};
 
 pub mod client;
 mod http;
@@ -10,6 +11,15 @@ pub mod server;
 pub trait Message: Send + Clone + Serialize {}
 
 impl<T: Send + Clone + Serialize> Message for T {}
+
+pub(crate) struct DebugMessage<'a, T: Message>(&'a T);
+
+impl<'a, T: Message> Debug for DebugMessage<'a, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let serialized = serde_json::to_string(self.0).unwrap();
+        f.write_str(&serialized)
+    }
+}
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Request<M: Message> {
