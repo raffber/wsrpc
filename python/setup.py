776aa038760941134e288113031eba38e329feec
Raphael Bernhard <beraphae@gmail.com>
2022-08-23 23:04:17 +0200

    More ci pipeline

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-with open("README.md") as f:
+with open("../README.md") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
+with open("../requirements.txt") as f:
     data = f.read()
 
-requirements = [line.strip() for line in data.split('\n') if line.strip() != '']
+requirements = [line.strip() for line in data.split("\n") if line.strip() != ""]
 
 setuptools.setup(
-    name="wsrpc",
+    name="broadcast_wsrpc",
     version="1.0.0",
     author="Raphael Bernhard",
     author_email="beraphae@gmail.com",
@@ -17,13 +17,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/raffber/wsrpc.git",
-    packages=['pywsrpc'],
+    packages=["broadcast_wsrpc"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.8",
     install_requires=requirements,
 )
-
