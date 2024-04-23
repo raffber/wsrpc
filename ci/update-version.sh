18092829cb3ab96350d7ba62edf6454a5f7f42ac
Raphael Bernhard <beraphae@gmail.com>
2024-04-23 17:26:29 +0200

    Fix update version script

--- ci/update-version.sh
+++ ci/update-version.sh
@@ -4,10 +4,9 @@ set -euxfo pipefail
 
 cd $(dirname "$0")/..
 
-changelog_version=$(grep -m 1 -oE '## \[.*?\]' CHANGELOG.md | sed -e 's/[# \[]//g' -e 's/\]//g')
+version=$(grep -m 1 -oE '## \[.*?\]' CHANGELOG.md | sed -e 's/[# \[]//g' -e 's/\]//g')
 
-sed -i "s/^version = \".*\"/version = \"$changelog_version\"/" rust/Cargo.toml
-sed -i "s/^version = \".*\"/version = \"$changelog_version\"/" pyproject.toml
-sed -i "s/^version: .*/version: $changelog_version/" dart/broadcast_wsrpc/pubspec.yaml
+rg '^version = \"[\d\.]+"' --iglob 'rust/Cargo.toml' --iglob "pyproject.toml" -m 1 -r "version = \"${version}\"" -q
+rg '^version: [\d\.]+' --iglob dart/broadcast_wsrpc/pubspec.yaml --iglob "pyproject.toml" -m 1 -r "version: ${version}" -q
 
 ./ci/check_version.sh
