e1ecb4b8db755760c7c6f350c4480c03e877adb0
Raphael Bernhard <beraphae@gmail.com>
2024-04-23 12:41:18 +0200

    More work on ci pipeline

--- ci/check_version.sh
+++ ci/check_version.sh
@@ -4,10 +4,10 @@ set -euxfo pipefail
 
 cd $(dirname "$0")/..
 
-rust_version=$(egrep -m 1 "^version" rust/Cargo.toml | cut -d "=" -f 2 | sed 's/[" ]//g')
+rust_version=$(grep -E -m 1 "^version" rust/Cargo.toml | cut -d "=" -f 2 | sed 's/[" ]//g')
 changelog_version=$(grep -m 1 -oE '## \[.*?\]' CHANGELOG.md | sed -e 's/[# \[]//g' -e 's/\]//g')
-python_version=$(egrep -m 1 "version" python/setup.py | cut -d "=" -f 2 | sed 's/[" ,]//g')
-dart_version=$(egrep -m 1 "version" dart/broadcast_wsrpc/pubspec.yaml | cut -d ":" -f 2 | sed 's/[ ]//g')
+python_version=$(grep -E -m 1 "version" pyproject.toml | cut -d "=" -f 2 | sed 's/[" ,]//g')
+dart_version=$(grep -E -m 1 "version" dart/broadcast_wsrpc/pubspec.yaml | cut -d ":" -f 2 | sed 's/[ ]//g')
 
 if [[ $rust_version != $changelog_version ]]; then
     echo "Invalid version in changelog"
--- python/broadcast_wsrpc/__init__.py
+++ python/broadcast_wsrpc/__init__.py
@@ -1,10 +1,11 @@
 from typing import Any, Dict, List
-from .client import Client
-from .server import Server
-
-
-__all__ = ["Client", "Server"]
 
 JsonDict = Dict[str, Any]
 JsonList = List[Any]
 JsonType = None | int | str | bool | JsonList | JsonDict
+
+from .client import Client  # noqa
+from .server import Server  # noqa
+
+
+__all__ = ["Client", "Server", "JsonDict", "JsonList", "JsonType"]
